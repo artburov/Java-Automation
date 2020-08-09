4c43a8da0fd3ed23639b669a86c569a606fac00f
Arthur Burov <art.burov@gmail.com>
2020-08-10 01:08:52 +0300

    added rest of old study examples, logs, and .properties file

---
+++ src/main/java/ExceptionStudy/exceptStudy.java
@@ -0,0 +1,5 @@
+package ExceptionStudy;
+
+public class exceptStudy {
+
+}
---
+++ src/main/java/StaticTest/SomeStatic.java
@@ -0,0 +1,9 @@
+package StaticTest;
+
+public class SomeStatic {
+
+    public static void myName(String someText){
+        String myName = someText;
+        System.out.println(myName);
+    }
+}
