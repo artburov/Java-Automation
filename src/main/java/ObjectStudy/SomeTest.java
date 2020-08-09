96db86f97617e285b7c4d5ed8355537cd1bb4579
Arthur Burov <art.burov@gmail.com>
2020-08-10 00:23:22 +0300

    Second commit with Object and constructor practice

---
+++ src/main/java/ObjectStudy/SomeTest.java
@@ -0,0 +1,34 @@
+package ObjectStudy;
+
+public class SomeTest {
+
+    private String myName;
+    private int age;
+
+
+    public SomeTest() {
+        System.out.println("Constructor test");
+        setName("sdf");
+    }
+
+    public SomeTest(int age) {
+        this.age = age;
+    }
+
+    public SomeTest(String myName) {
+         this.myName = myName;
+    }
+
+    public SomeTest setName(String name) {
+        this.myName = name;
+        return this;
+    }
+
+    public String getName() {
+        return myName;
+    }
+
+
+
+
+}
