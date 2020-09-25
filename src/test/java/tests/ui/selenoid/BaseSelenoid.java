21a03601f44cce336eeee31347d16f612c020596
Arthur Burov <art.burov@gmail.com>
2020-09-25 11:41:35 +0300

    to base selenoid class was added browser choice from the system property, also can be useful for Jenkins parametrization

--- src/test/java/tests/ui/selenoid/BaseSelenoid.java
+++ src/test/java/tests/ui/selenoid/BaseSelenoid.java
@@ -9,23 +9,32 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URI;
 
+import static helpers.ExcelReader.readProviderDataFromExcel;
+import static helpers.FileReader.loadDataForCreateIssuesProvider;
+
 public class BaseSelenoid {
     protected WebDriver driver;
-
+    protected DesiredCapabilities capabilities;
 
     @Before
     public void SetUp() throws MalformedURLException {
+        //Can be parametrized from Jenkins -> clean test -Dbrowser=${BROWSER}
+        System.setProperty("browser", "chrome");
+        String selectedBrowser = System.getProperty("browser");
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setVersion("84.0");
+        if (selectedBrowser.equals("firefox")) {
+            capabilities.setBrowserName("firefox");
+            capabilities.setVersion("79.0");
+        } else {
+            capabilities.setBrowserName("chrome");
+            capabilities.setVersion("84.0");
+        }
+
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", false);
-
         driver = new RemoteWebDriver(
-                URI.create("http://localhost:4444/wd/hub/").toURL(),
-                capabilities
-
-        );
+                URI.create("http://localhost:4444/wd/hub/").toURL(), capabilities);
     }
 
     @After
