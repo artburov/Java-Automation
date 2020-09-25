92361ac8d9bc31179676d5f505729282e7a2cb3d
Arthur Burov <art.burov@gmail.com>
2020-09-25 12:17:39 +0300

    1. Added switch statement for to choice browser for local tests run or from Jenkins 2. Removed useless imports

--- src/test/java/tests/ui/selenoid/BaseSelenoid.java
+++ src/test/java/tests/ui/selenoid/BaseSelenoid.java
@@ -9,32 +9,39 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URI;
 
-import static helpers.ExcelReader.readProviderDataFromExcel;
-import static helpers.FileReader.loadDataForCreateIssuesProvider;
-
 public class BaseSelenoid {
     protected WebDriver driver;
     protected DesiredCapabilities capabilities;
 
     @Before
     public void SetUp() throws MalformedURLException {
-        //Can be parametrized from Jenkins -> clean test -Dbrowser=${BROWSER}
-        System.setProperty("browser", "chrome");
+        /* Can be parametrized from Jenkins -> clean test -Dbrowser=${BROWSER} or just from edit configuration */
+
+        //Manual select browser, ex. firefox, chrome, opera into ""
+        System.setProperty("browser", "");
         String selectedBrowser = System.getProperty("browser");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        if (selectedBrowser.equals("firefox")) {
-            capabilities.setBrowserName("firefox");
-            capabilities.setVersion("79.0");
-        } else {
-            capabilities.setBrowserName("chrome");
-            capabilities.setVersion("84.0");
-        }
 
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", false);
-        driver = new RemoteWebDriver(
-                URI.create("http://localhost:4444/wd/hub/").toURL(), capabilities);
+        switch (selectedBrowser) {
+            case "firefox":
+                capabilities.setBrowserName("firefox");
+                capabilities.setVersion("79.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
+                driver = new RemoteWebDriver(
+                        URI.create("http://localhost:4444/wd/hub/").toURL(), capabilities);
+                break;
+            case "chrome":
+            default:
+                capabilities.setBrowserName("chrome");
+                capabilities.setVersion("84.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
+                driver = new RemoteWebDriver(
+                        URI.create("http://localhost:4444/wd/hub/").toURL(), capabilities);
+                break;
+        }
     }
 
     @After
